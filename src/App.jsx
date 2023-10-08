47195ca9229266de0ffc3e346046d4e1e143c9a4
yanitbee <yanithaftomm@gmail.com>
2023-10-08 01:44:24 -0700

    added ha.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
+import Ha from './components/ha';
 
 function App() {
+  return <div><Ha /></div>
   console.log(g);
 }
 export default App
---
+++ src/component/ha.jsx
@@ -0,0 +1,8 @@
+
+function Ha(){
+    return(
+        <span className="badge text-bg-info">Ha</span>
+    )
+    }
+    
+    export default Ha;
