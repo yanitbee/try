94694b7e6d892e17102a0ea27bfde5f37203aced
sosiabera <sosiabera32@gmail.com>
2023-10-22 10:45:33 -0300

    dsc

--- src/component/card.jsx
+++ src/component/card.jsx
@@ -1,138 +1,180 @@
-import { useRef, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useRef, useState,useEffect } from "react";
+import {useNavigate } from "react-router-dom";
 import "./card.css";
 import Slide from "@mui/material/Slide";
+import Buna from "./buna";
+import {FaLock} from "react-icons/fa"
 export default function LevelCard({ NewArr }) {
+  let totalvalue = 0;
   const refs = useRef();
   const reff = useRef();
   const refl = useRef();
+  const locks = useRef();
+  const lockf = useRef();
+  const lockl = useRef();
   const [hovers, sethovers] = useState(false);
   const [hoverf, sethoverf] = useState(false);
   const [hoverl, sethoverl] = useState(false);
+  useEffect(() => {
+    if(totalvalue<NewArr[0].min){
+      locks.current.style.filter="blur(6px)"
+    }
+    else{
+      locks.current.style.filter="blur(0)"
+    }
+    if(totalvalue<NewArr[1].min){
+      lockf.current.style.filter="blur(6px)"
+    }
+    else{
+      lockf.current.style.filter="blur(0)"
+    }
+    if(totalvalue<NewArr[2].min){
+      lockl.current.style.filter="blur(6px)"
+    }
+    else{
+      lockl.current.style.filter="blur(0)"
+    }
+  },[NewArr[0]])
   let navigate = useNavigate();
-  const handleclick = () => {
-    navigate("level", { state: "a" });
+  const handleclick = (level) => {
+    navigate("level", { state:  level});
   };
   return (
     <>
       <div className="parent">
-        <div className="container text-center">
-          <div className="row align-items-start">
-            <div className="col" onClick={handleclick}>
+        <div className="row align-items-start">
+          <div className="col ImgPlace"
+           onClick={()=>handleclick(NewArr[0].name)}>
+            <div className="levels">
               <h1 className="Levelname ">{NewArr[0].Level}</h1>
-              <div className="card">
-                <Link
-                  to={{
-                    pathname: "level",
-                    state: "a",
+          <div  ref={locks}>
+                <img
+                  className="CardImg  "
+                  src={NewArr[0].img}
+                  alt="..."
+                  ref={refs}
+                  onMouseEnter={() => {
+                    sethovers((prev) => !prev);
                   }}
-                >
-                  <img
-                    className="CardImg  "
-                    src={NewArr[0].img}
-                    alt="..."
-                    ref={refs}
-                    onMouseEnter={() => {
-                      sethovers((prev) => !prev);
-                    }}
-                    onMouseLeave={() => {
-                      sethovers((prev) => !prev);
-                    }}
-                  />
-                </Link>
-                <Slide
-                  direction="right"
-                  in={hovers}
-                  container={refs.current}
-                  mountOnEnter
-                  unmountOnExit
-                >
-                  <p
-                    ref={refs}
-                    onMouseEnter={() => {
-                      sethovers((prev) => !prev);
-                    }}
-                    onMouseLeave={() => {
-                      sethovers((prev) => !prev);
-                    }}
-                    className="place"
-                  >
-                    {NewArr[0].name}
-                  </p>
-                </Slide>
-              </div>
+                  onMouseLeave={() => {
+                    sethovers((prev) => !prev);
+                  }}
+                />
+            <div className="coffee">
+              <Buna completes={NewArr[0].q1complete} />
+              <Buna completes={NewArr[0].q2complete} />
+              <Buna completes={NewArr[0].q3complete} />
             </div>
-            <div className="col">
+            <Slide
+              direction="right"
+              in={hovers}
+              container={refs.current}
+              mountOnEnter
+              unmountOnExit
+            >
+              <p
+                ref={refs}
+                onMouseEnter={() => {
+                  sethovers((prev) => !prev);
+                }}
+                onMouseLeave={() => {
+                  sethovers((prev) => !prev);
+                }}
+                className="place"
+              >
+                {NewArr[0].name}
+              </p>
+            </Slide>
+            </div>
+            <div className="lock">
+            <FaLock />
+            </div>
+            </div>
+          </div>       
+          <div className="col ImgPlace" onClick={()=>handleclick(NewArr[1].name)}>
+            <div className="levels">
               <h1 className="Levelname">{NewArr[1].Level}</h1>
-              <div className="card">
-                <img
-                  className="CardImg"
-                  src={NewArr[1].img}
-                  alt="..."
-                  ref={reff}
-                  onMouseEnter={() => {
-                    sethoverf((prev) => !prev);
-                  }}
-                  onMouseLeave={() => {
-                    sethoverf((prev) => !prev);
-                  }}
-                />
-                <Slide
-                  direction="right"
-                  in={hoverf}
-                  container={reff.current}
-                  mountOnEnter
-                  unmountOnExit
-                >
-                  <p
-                    onMouseEnter={() => {
-                      sethoverf((prev) => !prev);
-                    }}
-                    onMouseLeave={() => {
-                      sethoverf((prev) => !prev);
-                    }}
-                    className="place"
-                  >
-                    {NewArr[1].name}
-                  </p>
-                </Slide>
-              </div>
+              <div  ref={lockf}>
+              <img
+                className="CardImg"
+                src={NewArr[1].img}
+                alt="..."
+                ref={reff}
+                onMouseEnter={() => {
+                  sethoverf((prev) => !prev);
+                }}
+                onMouseLeave={() => {
+                  sethoverf((prev) => !prev);
+                }}
+              />
+            <div className="coffee">
+              <Buna completes={NewArr[1].q1complete} />
+              <Buna completes={NewArr[1].q2complete} />
+              <Buna completes={NewArr[1].q3complete} />
             </div>
-            <div className="col">
+            <Slide
+              direction="right"
+              in={hoverf}
+              container={reff.current}
+              mountOnEnter
+              unmountOnExit
+            >
+              <p
+                onMouseEnter={() => {
+                  sethoverf((prev) => !prev);
+                }}
+                onMouseLeave={() => {
+                  sethoverf((prev) => !prev);
+                }}
+                className="place"
+              >
+                {NewArr[1].name}
+              </p>
+            </Slide>
+            </div>
+            </div>
+          </div>
+          <div className="col ImgPlace" onClick={()=>handleclick(NewArr[2].name)}>
+            <div className="levels">
               <h1 className="Levelname">{NewArr[2].Level}</h1>
-              <div className="card">
-                <img
-                  className="CardImg"
-                  src={NewArr[2].img}
-                  alt="..."
-                  ref={refl}
-                  onMouseEnter={() => {
-                    sethoverl((prev) => !prev);
-                  }}
-                  onMouseLeave={() => {
-                    sethoverl((prev) => !prev);
-                  }}
-                />
-                <Slide
-                  direction="right"
-                  in={hoverl}
-                  container={refl.current}
-                  mountOnEnter
-                  unmountOnExit
-                >
-                  <p
-                    onMouseEnter={() => {
-                      sethoverl((prev) => !prev);
-                    }}
-                    onMouseLeave={() => {
-                      sethoverl((prev) => !prev);
-                    }}
-                    className="place"
-                  >
-                    {NewArr[2].name}
-                  </p>
-                </Slide>
-              </div>
+              <div  ref={lockl}>
+              <img
+                className="CardImg"
+                src={NewArr[2].img}
+                alt="..."
+                ref={refl}
+                onMouseEnter={() => {
+                  sethoverl((prev) => !prev);
+                }}
+                onMouseLeave={() => {
+                  sethoverl((prev) => !prev);
+                }}
+              />
+            <div className="coffee">
+              <Buna completes={NewArr[2].q1complete} />
+              <Buna completes={NewArr[2].q2complete} />
+              <Buna completes={NewArr[2].q3complete} />
+            </div>
+            <Slide
+              direction="right"
+              in={hoverl}
+              container={refl.current}
+              mountOnEnter
+              unmountOnExit
+            >
+              <p
+                onMouseEnter={() => {
+                  sethoverl((prev) => !prev);
+                }}
+                onMouseLeave={() => {
+                  sethoverl((prev) => !prev);
+                }}
+                className="place"
+              >
+                {NewArr[2].name}
+              </p>
+            </Slide>
+            </div>
             </div>
           </div>
         </div>
--- src/pages/full.jsx
+++ src/pages/full.jsx
@@ -1,45 +1,69 @@
 import { useState } from "react";
 import LevelCard from "../component/card";
 import Arrow from "../component/arrow";
-
+import {Helmet, HelmetProvider} from 'react-helmet-async';
 function Full() {
   //the database
   let ImgProp = [
     {
-      name: "ብሄራዊ",
+      name: "ብሔራዊ",
       img: "/image/final beharaw1.png",
       id: "first",
       Level: "ሀ",
+      q1complete:100,
+      q2complete:100,
+      q3complete:100,
+      min:0
     },
     {
       name: "መርካቶ",  
       img: "/image/markato1.png",
       id: "second",
       Level: "ለ",
+      q1complete:100,
+      q2complete:100,
+      q3complete:34,
+      min:0
     },
     {
       name: "ፒያሳ",
       img: "/image/piassa.png",
       id: "third",
-      Level: "ሕ",
+      Level: "ሐ",
+      q1complete:25,
+      q2complete:56,
+      q3complete:100,
+      min:0
     },
     {
       name: "4ኪሎ",
       img: "/image/4kilo.png",
       id: "first",
-      Level: "a",
+      Level: "መ",
+      q1complete:34,
+      q2complete:100,
+      q3complete:0,
+      min:300
     },
     {
       name: "ብሄራዊ",
       img: "/image/g.jpg",
       id: "second",
-      Level: "l",
+      Level: "ረ",
+      q1complete:0,
+      q2complete:0,
+      q3complete:0,
+      min:600
     },
     {
       name: "ብሄራዊ",
       img: "/image/final beharaw.png",
       id: "third",
-      Level: "g",
+      Level: "ሰ",
+      q1complete:100,
+      q2complete:67,
+      q3complete:24,
+      min:900
     },
   ];
   //end level
@@ -72,10 +96,15 @@ function Full() {
   }
   return (
     <>
-      <div>
+    <HelmetProvider>
+      <div style={{ backgroundcolor: "#5e787d",   height: "100dvh"}}>
+      <Helmet>
+                <style>{'body { background-color: #5e787d; }'}</style>
+            </Helmet>
         <Arrow slide={setslide} />
         <LevelCard NewArr={NewArr} />
       </div>
+      </HelmetProvider>
     </>
   );
 }
